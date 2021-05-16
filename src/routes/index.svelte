9444f4d814a8a1d5665acb6166bb786eedc05d45
Sean aye <hello@seanaye.ca>
2021-05-16 17:50:06 -0400

    init commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,5 @@
+<script>
+  import "../app.postcss"
+</script>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a class="text-blue-600 underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
